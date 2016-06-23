b3d26f59ee2d292a08029564c0aee702c4f3746d
Murali Golivada <Murali Golivada>
2016-06-22 20:14:32 -0400

    added dependencies to pom.xml

--- src/test/java/GoogleExampleIT.java
+++ src/test/java/GoogleExampleIT.java
@@ -1,13 +1,28 @@
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 /*
  * Requirement:
  * 
  *  1. please fix all the bug in this file 
  *  2. please run it
  *  3. sent your test result and the fixed the file to us
- * 
+ * ***
  * 
  * */
-
+// Added dependencies and resolved issues for testing
+// Prodigy test for automation testing 
 
 @Test(groups={"localTest"})
 public class GoogleExampleIT {
@@ -30,14 +45,14 @@ public class GoogleExampleIT {
 		searchBar.clear();
 		searchBar.sendKeys("Milk!");
 		searchBar.sendKeys(Keys.ENTER);
-		LOGGER.info("Page title is: {}", driver.getTitle());
+		LOGGER.info("Page title is: {}"+driver.getTitle());
 		Assert.assertTrue(driver.getTitle().equals("Google"));
 	}
 
 	@BeforeMethod
 	public void setUp() {
 		
-		driver = new new FirefoxDriver();
+		driver = new  FirefoxDriver();
 		PageFactory.initElements(new AjaxElementLocatorFactory(driver, 60), this);
 		driver.get("http://www.google.com");
 	}
